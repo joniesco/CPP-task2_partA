8d7f8368838ea21fb3a1b233d956e1e29db126b7
jonathan escojido <73218886+joniesco@users.noreply.github.com>
2021-03-23 01:04:46 +0200

    final

---
+++ Board.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Board.hpp"
+using namespace std;
+
+namespace ariel {
+//--------------------------------------------post function------------------------------------------------------
+    void Board::post(int row, int column, Direction direction, string message) {
+       
+    }
+//--------------------------------------------read function------------------------------------------------------
+    string Board::read(int row, int column, Direction direction, int length) {
+        string ans= "";
+        
+        return ans;
+    }
+//-------------------------------------------show function-------------------------------------------------------
+    void Board::show() {
+        
+}
+}
